3133d79ff99207635e4fe0b109b62faf8e42e93c
Luke <lmatsui@berkeley.edu>
2025-10-30 21:34:46 +0900

    vulnerable2.c Done

--- [Working] vulnerable_C/vulnerable2.c
+++ [Working] vulnerable_C/vulnerable2.c
@@ -9,7 +9,10 @@ void func1(){
     
     printf("Enter Input : ");
     
-    while((c = getchar()) != '\n' && c != EOF) {
+    // VULNERABILITY: No bounds checking in while loop can lead to Buffer Overflow
+    // while((c = getchar()) != '\n' && c != EOF) {
+    // SOLUTION: Add bounds checking
+    while(i < sizeof(buffer) - 1 && (c = getchar()) != '\n' && c != EOF) {
         buffer[i++] = c;
     }
     buffer[i] = '\0';
@@ -24,18 +27,29 @@ void func2(){
     printf("Enter Input : ");
     fgets(input, sizeof(input), stdin);
     
-    sprintf(buffer, "User input: %s", input);
+    // VULNERABILITY: vulnerable function sprintf() to Buffer Overflow
+    // sprintf(buffer, "User input: %s", input);
+    // SOLUTION: Use safe function snprintf() instead with bounds checking
+    snprintf(buffer, sizeof(buffer), "User input: %s", input);
     
     printf("Input : %s\n", buffer);
 }
+
 void func3(){
     char buffer[8];
     char input[10];
     
     printf("Enter Input : ");
-    scanf("%s",input);
+    // VULNERABILITY: scanf("%s") without length limit vulnerable to Buffer Overflow
+    // scanf("%s",input);
+    // SOLUTION: Use scanf() with field width specifier
+    scanf("%9s", input);
     
-    strcpy(buffer, input);
+    // VULNERABILITY: vulnerable function strcpy() to Buffer Overflow
+    // strcpy(buffer, input);
+    // SOLUTION: Use safe function strncpy() with length limit
+    strncpy(buffer, input, sizeof(buffer) - 1);
+    buffer[sizeof(buffer) - 1] = '\0';
     
     printf("Copied : %s\n", buffer);
 }
